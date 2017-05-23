0d9404f8d11742340416d6f9ddbafa4d8c28e96d
Chermaine Cheang <chermaine@users.noreply.github.com>
2017-05-23 17:12:24 -0400

    javascript

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var input = "document.forms[0].element";
+function add(event) {
+	var type = event.target.parentNode.firstElementChild.value;
+	
+	if (type == "text")
+		handleText(type);
+	else if (type == input.button)
+		handleButton();
+	else if (type == input.radio)
+		handleRadio();
 
+	event.preventDefault();
+}
 
-async function handleText(type) {
-
+function handleText(type) {
 	console.log("handle text called\n");
 
 	//Create an input type dynamically.
@@ -13,12 +22,20 @@ async function handleText(type) {
 	element.setAttribute("type", type);
 	element.setAttribute("value", type);
 	element.setAttribute("name", type);
-
+	element.setAttribute("id", "newText");
+	element.rows="10";
+	element.cols="50";
 
 	var foo = document.getElementById("fooBar");
+	var br = document.createElement("br");
 
 	//Append the element in page (in span).
-	foo.appendChild(element);
+	newDiv.appendChild(element);
+	newDiv.appendChild(br);
+	createButton(newDiv, "Cancel");
+	createButton(newDiv, "Submit");
+	foo.appendChild(newDiv);
+	event.preventDefault();
 }
 
 function handleButton() {}
@@ -26,15 +43,26 @@ function handleButton() {}
 
 function handleRadio() {}
 
-function add(type) {
 
-	if (type == "text")
-		handleText(type);
-	else if (type == input.button)
-		handleButton();
-	else if (type == input.radio)
-		handleRadio();
+function createButton(parent, type) {
+	//create a button and set it attributes
+	var button = document.createElement("input");
+	button.setAttribute("id", type);
+	button.setAttribute("type", "button");
+	button.setAttribute("value", type);
 
+	//add event listener according to button type
+	if (type === "Cancel")
+		button.addEventListener("click", deleteElement(event));
+	if (type === "Submit")
+		button.addEventListener("click", addContent(event));
 
-
+	//append button 
+	parent.appendChild(button);
 }
+
+function addContent(event) {}
+
+function deleteElement(event) {}
+
+
